e63645b4acc1d5a00ceab462d9a4ec67a40dbe50
Innexafit Developer <developer@innexafit.local>
2026-06-09 16:26:36 +0300

    Fix OOM errors during Next.js build

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   typescript: {
     ignoreBuildErrors: true,
+  },
+  experimental: {
+    cpus: 1,
+    workerThreads: false,
   },
 };
 
